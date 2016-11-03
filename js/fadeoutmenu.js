ef5c393e244607a8ca74480d6e5042ba24b6dd61
Jørgen L. Hansen <Jrgen L. Hansen>
2016-11-03 00:51:54 +0000

    made everything align and added social media

--- js/fadeoutmenu.js
+++ js/fadeoutmenu.js
@@ -1,13 +1,14 @@
-
+var openWidth = 250;
+var closedWidth = 50;
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-    document.getElementByTag("header").style.marginLeft = "300px";
-    alert(document.getElementByTag("header").offsetWidth);
+    document.getElementById("mySidenav").style.width = openWidth+"px";
+    document.getElementById("header").style.left = openWidth+"px";
+    //alert(document.getElementsByTagName("header").offsetWidth);
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "50px";
-    document.getElementByTag("header").style.marginLeft = "50px";
+    document.getElementById("mySidenav").style.width = closedWidth+"px";
+    document.getElementById("header").style.left = closedWidth+"px";
 }
